b62dc271ddba4eebbe0b27fafe3eb283a1ed9b20
User <senamela@wamly.io>
2022-09-17 07:27:06 +0200

    functions.py

---
+++ function.py
@@ -0,0 +1,9 @@
+input_val = "fr"
+
+def greeting(lang):
+    if lang == "en":
+        return "Hello, World!"
+    elif lang == "fr":
+        return "Bonjour le Monde!"
+    else:
+        return "Language Not Supported"
