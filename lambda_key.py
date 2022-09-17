c378936ee105d016af323780be0cd49b9b2e9553
User <senamela@wamly.io>
2022-09-17 07:53:16 +0200

    lambda_key.py, Avacado bowl, context

--- lambda_key.py
+++ lambda_key.py
@@ -1,7 +1,7 @@
 order = [
     ["Greek Salad", 84.00, 3],
     ["Chicken Wrap", 99.00, 2],
-    ["Avocado", 78.00, 1]
+    ["Avocado Bowl", 78.00, 1]
 ]
 
 sorted_order = sorted(order, key=lambda x: x[1])
