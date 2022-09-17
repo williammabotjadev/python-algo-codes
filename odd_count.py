c35a1649845e168f4deab708747898bdd15f015d
User <senamela@wamly.io>
2022-09-17 11:49:17 +0200

    odd_count.py

---
+++ odd_count.py
@@ -0,0 +1,13 @@
+# Odd count Generator Function
+
+def oddCount(min, max):
+    while min <= max:
+        yield min
+        min += 2
+
+# For Loop to print odd numbers
+
+for i in oddCount(0, 10):
+    print(i)
+
+    
