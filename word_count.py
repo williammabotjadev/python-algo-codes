a634cfe9a40685cb3943e9b98e145d48190b4a2b
User <senamela@wamly.io>
2022-09-17 16:07:36 +0200

    word_count.txt, word_count.py

---
+++ word_count.py
@@ -0,0 +1,23 @@
+# Using a dictionary for word count
+
+def wordcount(fname):
+
+    try:
+        f = open(fname)
+    except:
+        print("File Cannot be opened")
+        exit()
+
+    count = dict()
+
+    for line in f:
+        words = line.split()
+        for word in words:
+            if word not in words:
+                count[word] = 1
+            else:
+                count[word] += 1     
+    return count 
+
+
+            
