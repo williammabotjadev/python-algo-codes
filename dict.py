d8239bda2306c3ad8bfeed33af4bf4dc09a140a9
User <senamela@wamly.io>
2022-09-17 15:39:43 +0200

    dict.py, sorting

--- dict.py
+++ dict.py
@@ -20,3 +20,13 @@ d.update({
 print("five" in d)
 
 print(d)
+
+# Get sorted list of keys
+
+sorted_keys = sorted(list(d))
+
+print(sorted_keys)
+
+sorted_vals = sorted(list(d.values()))
+
+print(sorted_vals)
