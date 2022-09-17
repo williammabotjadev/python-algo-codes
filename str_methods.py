76cdf765b11da837b6745cef2eb120f05d15a4db
User <senamela@wamly.io>
2022-09-17 05:53:11 +0200

    str_methods, find method

--- str_methods.py
+++ str_methods.py
@@ -7,3 +7,9 @@ print(test.count("ab"))
 test_one = "A\tBrown\tQuick\tFox"
 
 print(test_one.expandtabs(1))
+
+test_two = "The man on the moon"
+
+print(test_two.find("man"))
+print(test_two.find("woman"))
+
