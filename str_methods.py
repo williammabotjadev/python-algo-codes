e99a97964762c293e94c8b9ecd468dd3876c1028
User <senamela@wamly.io>
2022-09-17 06:01:34 +0200

    str_methods.py, join methods

--- str_methods.py
+++ str_methods.py
@@ -34,3 +34,8 @@ for j in test_five:
 
 print("\n")
 
+list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+res = "".join(list)
+
+print(res)
