bdb7cea743a9b04e8cc61be9f33fba91f41cf521
User <senamela@wamly.io>
2022-09-17 14:43:34 +0200

    class_methods.py, Baexp class

--- class_methods.py
+++ class_methods.py
@@ -6,6 +6,12 @@ class Aexp(object):
     def exp(cls, x):
         return(cls.base ** x)
 
+class Baexp(Aexp):
+    base = 3 
+
 if __name__ == "__main__":
     exp = Aexp()
     print(exp.exp(2))
+
+    bexp = Baexp()
+    print(bexp.exp(2))
