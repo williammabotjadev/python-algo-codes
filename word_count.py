1af839ebfcbf6f5978c6b6dc509523e44c126f70
User <senamela@wamly.io>
2022-09-17 16:17:48 +0200

    word_count.py, bug fixed, not in count

--- word_count.py
+++ word_count.py
@@ -3,21 +3,21 @@
 def wordcount(fname):
 
     try:
-        f = open(fname)
+        fhand = open(fname)
     except:
         print("File Cannot be opened")
         exit()
 
     count = dict()
 
-    for line in f:
+    for line in fhand:
         words = line.split()
         for word in words:
-            if word not in words:
+            if word not in count:
                 count[word] = 1
             else:
                 count[word] += 1     
-    return count 
+    return(count) 
 
-print(wordcount("word_count.txt"))
+print(wordcount('word_count.txt'))
             
