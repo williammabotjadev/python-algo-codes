028866de4f1037824cd46bd2901d2404f168a243
User <senamela@wamly.io>
2022-09-17 07:59:58 +0200

    iter_recursive.py, base case

--- iter_recursion.py
+++ iter_recursion.py
@@ -4,9 +4,11 @@ def iterTest(min, max):
         min += 1
 
 def recursiveTest(min, max):
+    if min <= 0:
+        return 
     if min <= max:
         print(min)
         return recursiveTest(min + 1, max)
 
 iterTest(0, 10)
-recursiveTest(0, 10)
+recursiveTest(1, 10)
