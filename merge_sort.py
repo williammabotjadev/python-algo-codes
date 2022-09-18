5140c7e7e49712f3015b2fcf73568ecabb988209
User <senamela@wamly.io>
2022-09-18 15:22:59 +0200

    merge_sort.py

---
+++ merge_sort.py
@@ -0,0 +1,44 @@
+# Python merge sort implementation
+
+def mergesort(arr):
+
+    # Base Case 
+
+    if len(arr) <= 1:
+        return 
+
+    # Find the Mid point
+
+    mid = int(len(arr)//2)
+
+    # Partition the main array 
+
+    left = arr[:mid]
+    right = arr[mid:]
+
+    # Recursively run mergesort on each side
+
+    mergesort(left)
+    mergesort(right)
+
+    # Swap items based size on either sides 
+
+    i = j = k = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            arr[k] = left[i]
+            i += 1 
+        else:
+            arr[k] = right[j]
+            j += 1 
+
+    while i < len(left):
+        arr[k] = left[i]
+        k += 1
+        i += 1 
+
+    while j < len(right):
+        arr[k] = right[j]
+        k += 1
+        j += 1 
