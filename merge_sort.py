26f2474fbd277f0b2889332482606a655c24db0e
User <senamela@wamly.io>
2022-09-18 15:34:09 +0200

    merge_sort.py, function context

--- merge_sort.py
+++ merge_sort.py
@@ -26,12 +26,14 @@ def mergesort(arr):
     i = j = k = 0
 
     while i < len(left) and j < len(right):
+        
         if left[i] < right[j]:
             arr[k] = left[i]
             i += 1 
         else:
             arr[k] = right[j]
             j += 1 
+        k += 1
 
     while i < len(left):
         arr[k] = left[i]
