ed174f1256a930e59b1abe2e13a887ebdb7890ef
User <senamela@wamly.io>
2022-09-18 16:21:19 +0200

    log_graph.py

---
+++ log_graph.py
@@ -0,0 +1,15 @@
+# A graph representation of Logarithmic vs. Constant time 
+
+import matplotlib.pyplot as plt 
+from math import log 
+
+x = list(range(1, 100))
+
+lst_one = []
+lst_two = []
+
+a = 1 
+
+plt.plot(x, [y * y for y in x ])
+plt.plot(x, [(7 * y) * log(y, 2) for y in x])
+plt.show()
